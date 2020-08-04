a1ef47789ef9585ca95479560630cbdd04cf8cea
PrettyScript <JessicaDelcy@gmail.com>
2020-08-03 21:30:20 -0500

    removed the inventory component from index file, it's not being used there

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import InventoryManagement from "./components/admin/InventoryManagement";
 
 ReactDOM.render(
     <React.StrictMode>
