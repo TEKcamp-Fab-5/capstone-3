3ff6e616756a20cf4c0bbae7988dc5bfabfaed9c
Justin <ijustin17@gmail.com>
2020-08-06 15:50:59 -0500

    added inventory management table column: SKU

--- frontend/src/components/admin/InventoryView.js
+++ frontend/src/components/admin/InventoryView.js
@@ -4,6 +4,16 @@ import Paper from "@material-ui/core/Paper";
 export default function InventoryView(props) {
     const { products, setProducts } = props;
 
+    const displayListOfProductSerial = products.map(product => {
+        return (
+            <div>
+                <tr>
+                    <td>{product.sku}</td>
+                </tr>
+            </div>
+        )
+    })
+
     const displayListOfProductName = products.map(product => {
         return (
             <div>
@@ -28,9 +38,11 @@ export default function InventoryView(props) {
             <Paper style={{ padding: "2rem" }}>
                 <table>
                     <tr>
+                        <th>Product SKU</th>
                         <th>Product Name</th>
                         <th>Product Quantity</th>
                     </tr>
+                    <td>{displayListOfProductSerial}</td>
                     <td>{displayListOfProductName}</td>
                     <td>{displayListOfProductQuantity}</td>
                 </table>
