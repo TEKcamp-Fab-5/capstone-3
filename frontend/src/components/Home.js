fb4145cba6a7b5f143efb47518b1c1dceffd4dfa
PrettyScript <JessicaDelcy@gmail.com>
2020-08-05 23:52:16 -0500

    removed unnecessary code, it was for testing purposes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -17,18 +17,6 @@ export default function Home(props) {
     // Home: the search bar is displayed on this page(component), the first if check
     // is to check the input value, if input value is greater than 0 and the products that are filtered equal 0 (that product is not available). If the product name is available then the corresponding products will display.
 
-    const handleProducts = () => {
-        BoutiqueDataService.retrieveAllProducts()
-            .then(res => {
-                console.log(res);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
-
-    handleProducts();
-
     // Home: listOfProducts (inventory) is mapped by the product, the product component wrapped in
     // a dynamic link will display the image, the price and an add to cart button.
     const displayProducts = listOfProducts => {
